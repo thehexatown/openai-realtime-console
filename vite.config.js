c4802e27b2b5656867c39f1a44d2e354f9a33b12
Zaryab Sundhu <143507375+thehexatown-zaryab-sundhu@users.noreply.github.com>
2026-01-01 19:06:11 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,7 @@ const path = fileURLToPath(import.meta.url);
 export default {
   root: join(dirname(path), "client"),
   plugins: [react()],
+  server: {
+    allowedHosts: ["openai-console.chillkro.com"],
+  },
 };
